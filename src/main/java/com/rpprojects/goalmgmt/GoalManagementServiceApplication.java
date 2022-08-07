2b24facb6b536840614d090e8cd88a0280a55e15
Rajagopalan Padmanaban Anand <rajagopalan2712@gmail.com>
2022-08-07 09:45:46 -0500

    Initial Commit

---
+++ src/main/java/com/rpprojects/goalmgmt/GoalManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rpprojects.goalmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalManagementServiceApplication.class, args);
+	}
+
+}
